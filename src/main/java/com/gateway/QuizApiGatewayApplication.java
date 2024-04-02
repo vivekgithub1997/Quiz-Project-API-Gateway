ba191bc96d5a941aa66bd4e3f1bb49e08e982466
vivek raikawar <vivekckt1997@gmail.com>
2024-04-02 22:03:50 +0530

    first commit

---
+++ src/main/java/com/gateway/QuizApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApiGatewayApplication.class, args);
+	}
+
+}
